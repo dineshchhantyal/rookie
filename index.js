05fecea566e8edbfc07dba8042a7b9127550323d
Dinesh Chhantyal <myagdichhantyal@gmail.com>
2021-05-18 11:54:42 +0545

    Testing heroku

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
+const express = require("express");
 const Discord = require("discord.js");
-const client = new Discord.Client();
 require("dotenv").config();
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-const reply = [
-  "Aafu Chahi Padni Malai Kaam Nadine? ✍",
-  "Malaii Jhau Layo 😴",
-  "I lop U 💘",
-  "Stay Safe 🦺",
-  "K Cha Nanu Timro Kharab 👄",
-];
-client.on("message", (msg) => {
-  const msg_lower = msg.content.toLowerCase();
-  var reply_code = Math.floor(Math.random() * reply.length);
+const client = new Discord.Client();
+const app = express();
 
-  if (msg_lower.includes("roo!")) {
-    msg.reply(reply[reply_code]);
-  }
+app.get("/", function (req, res) {
+  res.status(200).json({ message: "Hello" });
 });
 
-client.login(process.env.ACESS_TOKEN);
+const Bot = () => {
+  client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  });
+  const reply = [
+    "Aafu Chahi Padni Malai Kaam Nadine? ✍",
+    "Malaii Jhau Layo 😴",
+    "I lop U 💘",
+    "Stay Safe 🦺",
+    "K Cha Nanu Timro Kharab 👄",
+  ];
+  client.on("message", (msg) => {
+    const msg_lower = msg.content.toLowerCase();
+    var reply_code = Math.floor(Math.random() * reply.length);
+
+    if (msg_lower.includes("roo!")) {
+      msg.reply(reply[reply_code]);
+    }
+  });
+
+  client.login(process.env.ACESS_TOKEN);
+};
+
+Bot();
+app.listen(process.env.PORT);
