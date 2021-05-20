1616b4980ef98a0bb09098021fdd2413b01baf89
Dinesh Chhantyal <myagdichhantyal@gmail.com>
2021-05-20 17:09:42 +0545

    Database message to everyone

--- scripts/createProfile.js
+++ scripts/createProfile.js
@@ -1,32 +1,30 @@
 const Users = require("./Schema/userSchema");
 
 const addNewUser = () => {
-  Users.create(
-    {
-      name: "Dinesh",
-      number: [9866042400, 98606042400],
-      message: "Work and React ♥",
-      interest: ["Web Development"],
-      interest: [
-        "Web Development",
-        "JavaScript",
-        "React",
-        "Node.js",
-        "Python Thingy",
-        "Selenium",
-        "Django",
-      ],
-      workingOn: ["Personal Portfiolio", "077 BCT Control System"],
-      maritalStatus: true,
-    },
-    (err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(data);
-      }
+  // sample  {
+  //   name: "Dinesh",
+  //   number: [9866042400, 98606042400],
+  //   message: "Work and React ♥",
+  //   interest: ["Web Development"],
+  //   interest: [
+  //     "Web Development",
+  //     "JavaScript",
+  //     "React",
+  //     "Node.js",
+  //     "Python Thingy",
+  //     "Selenium",
+  //     "Django",
+  //   ],
+  //   workingOn: ["Personal Portfiolio", "077 BCT Control System"],
+  //   maritalStatus: true,
+  // }
+  Users.create(msg.content, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
     }
-  );
+  });
 };
 
 module.exports = addNewUser;
