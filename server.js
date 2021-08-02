27586041d3950fab51745c0b43d284b28b089bfb
Dinesh Chhantyal <myagdichhantyal@gmail.com>
2021-08-02 15:49:28 +0545

    BUD- DB Fix

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use(morgan("common"));
 app.use(helmet());
 app.use(cors());
 if (build == "prod") {
-   dbConnection = `mongodb+srv://077-BCT:${process.env.DBPASSWORD}@cluster0.r7knx.mongodb.net/studentCollection?retryWrites=true&w=majority`;
+  dbConnection = `mongodb+srv://rookie:${process.env.DBPASSWORD}@cluster0.kjvqe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+  `;
 } else {
   dbConnection = `mongodb://localhost:27017/rookie`;
 }
@@ -34,35 +35,35 @@ mongoose.connect(
 );
 
 
-  mongoose.connection.once("open", () => {
-    console.log("DB is connected");
-  });
+mongoose.connection.once("open", () => {
+  console.log("DB is connected");
+});
 
 app.post("/webhook", async (req, res) => {
-     const Payload = req.body;
-    //Respond To Heroku Webhook
-     res.sendStatus(200);
+  const Payload = req.body;
+  //Respond To Heroku Webhook
+  res.sendStatus(200);
 
-    const options = {
+  const options = {
     method: "POST",
     url:
-     process.env.DISCORDHOOKURL,
+      process.env.DISCORDHOOKURL,
     headers: {
-     "Content-type": "application/json",
+      "Content-type": "application/json",
     },
-        //Format JSON DATA
+    //Format JSON DATA
     body: JSON.stringify({
-     content: `This is A Webhook notification!A build for your app ${Payload.data.app.name} was just triggered`,
+      content: `This is A Webhook notification!A build for your app ${Payload.data.app.name} was just triggered`,
     }),
-    };
-    request(options, function (error, response) {
+  };
+  request(options, function (error, response) {
     if (error) throw new Error(error);
     console.log(response);
-    });
+  });
 });
 Bot();
 app.use("/team", team);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`listening at http://localhost:${port}`);
 });
