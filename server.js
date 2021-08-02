54fa354676a2e065759f4cca0fc642b444ef2ade
Dinesh Chhantyal <69712191+dineshchhantyal@users.noreply.github.com>
2021-08-02 13:41:43 +0545

    Heroku WEB HOOKS Added

--- server.js
+++ server.js
@@ -15,5 +15,26 @@ const Database = () => {
     console.log("DB is connected");
   });
 };
+app.post("/webhook", async (req, res) => {
+     const Payload = req.body;
+    //Respond To Heroku Webhook
+     res.sendStatus(200);
 
+     const options = {
+      method: "POST",
+      url:
+       "https://discord.com/api/webhooks/XXXXXXXXXXXXXX",
+      headers: {
+       "Content-type": "application/json",
+      },
+    //Format JSON DATA
+      body: JSON.stringify({
+       content: `This is A Webhook notification!A build for your app ${Payload.data.app.name} was just triggered`,
+      }),
+     };
+     request(options, function (error, response) {
+      if (error) throw new Error(error);
+      console.log(response);
+     });
+    });
 Bot();
